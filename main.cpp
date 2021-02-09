2be1fe38c95f204a1ee81f3d9cc96faaa096eced
Clinterpottrmus <Clinterpottrmus@gmail.com>
2021-02-08 20:52:25 -0900

    Part three, bool exist function

--- main.cpp
+++ main.cpp
@@ -12,17 +12,40 @@ using namespace std;
 
 void InputNames(vector<string> & names);
 
+bool DoesNameExist(const string & nameToFind, const vector<string> & names);
+
 int main(int argc, char **argv) {
     vector<string> names;
+    string nameToFind;
+
     InputNames(names);
+
+    cout << "Enter a name to search:";
+    cin >> nameToFind;
+
+    DoesNameExist(nameToFind, names);
+
     return 0;
 }
 
 void InputNames(vector<string> & names){
     for (int i = 0; i < 10; i++) {
         string name;
-        cout << "Please enter a name: ";
+        cout << "Please enter a name:";
         getline(cin, name);
         names.push_back(name);
     }
+}
+
+bool DoesNameExist(const string & nameToFind, const vector<string> & names){
+    for (int i = 0; i < names.size(); i++){
+        if (names[i] == nameToFind) {
+            cout << "true";
+            return true;
+        } else{
+
+        }
+    }
+    cout << "false";
+    return false;
 }
