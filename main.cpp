5d6cef02e1458966633be58654ff76fb1a0e2b46
Clinterpottrmus <Clinterpottrmus@gmail.com>
2021-02-08 20:18:52 -0900

    First base function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
+/* Lab 7
+ * Clint Scholtisek CS F201 FXA
+ * Jon Genetti
+ * Takes three inputs and prints them in a line*/
+
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "This is a test" << std::endl;
+    vector<string> names;
+    for (int i = 0; i < 10; i++) {
+        string name;
+        cout << "Please enter a name: ";
+        getline(cin, name);
+        names.push_back(name);
+    }
     return 0;
 }
